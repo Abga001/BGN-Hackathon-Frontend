bc2d7acfa36e83447859cf035b3f445a86a245af
Aaa Gmel <Abdullahagumel@gmail.com>
2022-10-29 11:49:05 +0100

    front end framework

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import styled from 'styled-components'
+import Header from './components/Header'
+import Sidebar from './components/Sidebar'
+import Feed from './components/Feed'
+
+function App() {
+  return (
+    
+    <div className="App">
+      <AppWrapper>
+      <Header />
+      <div className="app__body">
+      <Sidebar />
+      <Feed />
+      </div>
+      </AppWrapper>
+    </div>
+  );
+}
+
+const AppWrapper = styled.div`
+background-color: #f1f2f5;
+.app__body {
+  display: flex;
+}
+`
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyDaDKogp2LMjUBz2jrk5mFTAKXkD8NOp-0",
+  
+    authDomain: "popular-social-mern-8c4b3.firebaseapp.com",
+  
+    projectId: "popular-social-mern-8c4b3",
+  
+    storageBucket: "popular-social-mern-8c4b3.appspot.com",
+  
+    messagingSenderId: "607269875467",
+  
+    appId: "1:607269875467:web:fa5f5a95d5a3d9b65ebc0e"
+  
+  };
