fa30de6f4dde7cbeba85c8718c7bcf7c3ddfdc43
AustinBwanika <87410280+AustinBwanika@users.noreply.github.com>
2022-10-29 18:26:24 +0100

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCJzVEXHL900iuQYlLJIEaKv6rNdbfDkDE",
+
+  authDomain: "bgn-hack22lon-6506.firebaseapp.com",
+
+  projectId: "bgn-hack22lon-6506",
+
+  storageBucket: "bgn-hack22lon-6506.appspot.com",
+
+  messagingSenderId: "116285399780",
+
+  appId: "1:116285399780:web:890cfb767245469e2282f1",
+
+  measurementId: "G-BXL1E4T441"
+
+  
+};
