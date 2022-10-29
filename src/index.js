5227de0ac2a7ae6fea48401a88c3b1601607882d
kushal.q.patel <kushal.q.patel@pwc.com>
2022-10-29 18:42:34 +0100

    state code added

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// import { StateProvider } from './StateProvider';
+import reducer, { initialState } from './Reducer.js';
+
 ReactDOM.render(
-  
 <React.StrictMode>
 <App />
 </React.StrictMode>,
